416d91db16100221d943207ae907a73102165cb2
tainy-zhang <xiaofei.zhang@eventbank.com>
2014-12-30 15:02:11 +0800

    added new java activity

--- app/src/main/java/com/example/tainy/basefunc/MainActivity.java
+++ app/src/main/java/com/example/tainy/basefunc/MainActivity.java
@@ -15,12 +15,12 @@ public class MainActivity extends ActionBarActivity {
     }
 
 
-    @Override
+/*    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
-    }
+    }*/
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
